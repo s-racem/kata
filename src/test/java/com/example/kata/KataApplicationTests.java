9f9851c3e59117fd0583cd50f8438d05f2fc645f
Racem SMAOUI <sracem@gmail.com>
2017-08-12 00:23:46 +0200

    add new project

---
+++ src/test/java/com/example/kata/KataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.kata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
