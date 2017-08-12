323367119e4d3442bbfaefe4ce7072b04d36fc73
Racem SMAOUI <sracem@gmail.com>
2017-08-12 12:09:29 +0200

    test saving deposit transaction

--- src/test/java/com/example/kata/repository/TransactionRepositoryTest.java
+++ src/test/java/com/example/kata/repository/TransactionRepositoryTest.java
@@ -24,5 +24,6 @@ public class TransactionRepositoryTest {
                 .date(new Date())
                 .account(account)
                 .build();
+        transactionRepository.saveAndFlush(depositTransaction);
     }
 }
