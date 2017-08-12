8d63c8e5fcd50774a7e12fed256ab8f1365f71c3
Racem SMAOUI <sracem@gmail.com>
2017-08-12 14:46:03 +0200

    add transaction service

---
+++ src/main/java/com/example/kata/service/TransactionService.java
@@ -0,0 +1,14 @@
+package com.example.kata.service;
+
+import com.example.kata.domain.models.Transaction;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by sracem on 12/08/2017.
+ */
+@Service
+public class TransactionService {
+    public Transaction addTransaction(Transaction transaction) {
+        return null;
+    }
+}
