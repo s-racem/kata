c9d30d6a02255f5ef00307ffc015dd0fe7dbe781
Racem SMAOUI <sracem@gmail.com>
2017-08-12 13:49:22 +0200

    add find transaction by account method

--- src/main/java/com/example/kata/repository/TransactionRepository.java
+++ src/main/java/com/example/kata/repository/TransactionRepository.java
@@ -1,12 +1,16 @@
 package com.example.kata.repository;
 
+import com.example.kata.domain.models.Account;
 import com.example.kata.domain.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by sracem on 12/08/2017.
  */
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByAccount(Account account);
 }
