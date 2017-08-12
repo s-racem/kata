6cea758098e13b1f70b406c3bf9d5cd71d7694dd
Racem SMAOUI <sracem@gmail.com>
2017-08-12 11:25:07 +0200

    start add deposit transaction service

---
+++ src/test/java/com/example/kata/Constants.java
@@ -0,0 +1,8 @@
+package com.example.kata;
+
+/**
+ * Created by sracem on 12/08/2017.
+ */
+public class Constants {
+    public static final Long DEPOSIT_AMMOUNT = 100L;
+}
---
+++ src/test/java/com/example/kata/repository/TransactionRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.example.kata.repository;
+
+import com.example.kata.Constants;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+/**
+ * Created by sracem on 12/08/2017.
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class TransactionRepositoryTest {
+
+    @Test
+    public void should_add_deposit_transaction() throws Exception {
+        Transaction depositTransaction = Transaction.builder()
+                .amount(Constants.DEPOSIT_AMMOUNT)
+                .transactionType(TransactionType.DEPOSIT)
+                .date(new Date())
+                .account(account)
+                .build();
+    }
+}
