3a5aa6e8a6515f0feece82a8f0d640fb3828fdfd
Racem SMAOUI <sracem@gmail.com>
2017-08-12 15:58:15 +0200

    update test of add deposit transaction service

--- src/test/java/com/example/kata/service/TransactionServiceTest.java
+++ src/test/java/com/example/kata/service/TransactionServiceTest.java
@@ -4,22 +4,31 @@ import com.example.kata.Constants;
 import com.example.kata.domain.enums.TransactionType;
 import com.example.kata.domain.models.Account;
 import com.example.kata.domain.models.Transaction;
+import com.example.kata.repository.TransactionRepository;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
 
 /**
  * Created by sracem on 12/08/2017.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class TransactionServiceTest {
     @Spy
     @InjectMocks
     private TransactionService transactionService;
+
+    @Mock
+    private TransactionRepository transactionRepository;
 
     private Account account;
 
@@ -38,6 +47,7 @@ public class TransactionServiceTest {
                 .date(new Date())
                 .account(account)
                 .build();
+        doReturn(depositTransaction).when(transactionRepository).save(depositTransaction);
         Transaction result = transactionService.addTransaction(depositTransaction);
         assertThat(result).isEqualToComparingFieldByField(depositTransaction);
 
