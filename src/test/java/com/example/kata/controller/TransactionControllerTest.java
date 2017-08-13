ce23a8001f2689d9fe17842b2645ad8da06f8301
Racem SMAOUI <sracem@gmail.com>
2017-08-13 19:18:13 +0200

    add findTransactions controller test

--- src/test/java/com/example/kata/controller/TransactionControllerTest.java
+++ src/test/java/com/example/kata/controller/TransactionControllerTest.java
@@ -16,10 +16,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -39,6 +42,7 @@ public class TransactionControllerTest {
 
     private Account account;
     private Transaction depositTransaction;
+    private List<Transaction> transactions;
 
     @Before
     public void init() {
@@ -52,6 +56,8 @@ public class TransactionControllerTest {
                 .date(new Date())
                 .account(account)
                 .build();
+        transactions = new ArrayList<>();
+        transactions.add(depositTransaction);
     }
 
     @Test
@@ -61,5 +67,15 @@ public class TransactionControllerTest {
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .content(mapper.writeValueAsString(depositTransaction)))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void should_get_all_transactions_by_account() throws Exception {
+        doReturn(transactions).when(transactionService).findByAccount(account);
+        mockMvc.perform(post("/api/findByAccount")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(mapper.writeValueAsString(depositTransaction)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(mapper.writeValueAsString(transactions)));
     }
 }
